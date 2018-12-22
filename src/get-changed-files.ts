93ba79ddd9cb9018d829c7cf6f72891a36190a20
Evgeniy Timokhov <timocov@gmail.com>
2018-12-22 14:18:16 +0300

    Added simple implementation of filtering by checking last modified date

--- src/get-changed-files.ts
+++ src/get-changed-files.ts
@@ -1,3 +1,25 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as ts from 'typescript';
+
 export function getChangedFiles(inputFiles: string[]): string[] {
-	return inputFiles;
+	return inputFiles.filter((file: string) => {
+		const ext = path.extname(file);
+		if (ext === ts.Extension.Ts || ext === ts.Extension.Tsx) {
+			const jsFile = file.substring(0, file.length - ext.length) + ts.Extension.Js;
+
+			try {
+				const tsFileChangedTime = fs.statSync(file).mtimeMs;
+				const jsFileChangedTime = fs.statSync(jsFile).mtimeMs;
+
+				if (jsFileChangedTime > tsFileChangedTime) {
+					return false;
+				}
+			} catch {
+				// do nothing
+			}
+		}
+
+		return true;
+	});
 }
